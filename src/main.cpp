10d50776efa35c646397da12e8d883f4db6f9f70
hoangm960 <hoangm960@gmail.com>
2025-10-26 10:01:26 +0700

    remove task 6

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "temp_humi_monitor.h"
 #include "mainserver.h"
 #include "tinyml.h"
-#include "coreiot.h"
 
 
 void setup() {
@@ -16,7 +15,6 @@ void setup() {
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor" ,2048  ,NULL  ,2 , NULL);
   xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
   xTaskCreate( tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
-  xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,NULL  ,2 , NULL);
 }
 
 void loop() {
